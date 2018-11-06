7c4bc26c444e6d5fd950510965c7eb022bcc8e03
Laura Bloch <lbloch220@gmail.com>
2018-11-05 20:20:36 -0500

    now we're in business! plays Caroline when it sees the astronaut image

---
+++ Unity/DigitalDemo/Assets/PlayMusicOnTarget.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class PlayMusicOnTarget : MonoBehaviour, ITrackableEventHandler {
+
+    private TrackableBehaviour mTrackableBehavior;
+
+	// Use this for initialization
+	void Start () {
+        mTrackableBehavior = GetComponent<TrackableBehaviour>();
+        if(mTrackableBehavior)
+        {
+            mTrackableBehavior.RegisterTrackableEventHandler(this);
+        }
+	}
+	
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            // Stop audio when target is lost
+            GetComponent<AudioSource>().Pause();
+        }
+    }
+}
